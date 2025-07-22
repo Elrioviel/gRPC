e4c59a297a84211c20f5714c8c5611ae57c96e5e
Ghalia <ghaliadahech@gmail.com>
2025-07-22 16:13:11 +0300

    Added: document protobuf

--- 01-Distributed Real-Time Collaboration Platform/CollabApp/Program.cs
+++ 01-Distributed Real-Time Collaboration Platform/CollabApp/Program.cs
@@ -1,11 +1,35 @@
 using CollabApp.Server.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
 
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.TokenValidationParameters = new()
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+        };
+    });
+
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapGrpcService<CollaborationServiceImpl>();
 app.MapGet("/", () => "This is a gRPC service. Use a gRPC client to connect.");
